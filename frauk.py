9d43ff0e9d5967f2fc931bf3d10fa114f4b3c9c8
Vann <poniesofgore@gmail.com>
2017-06-26 19:57:10 +0200

    Added show_routes on route('/') - Lists all existing routes for dev/dbg

--- frauk.py
+++ frauk.py
@@ -1,5 +1,5 @@
 import sqlite3, os
-from flask import Flask, request, g, render_template, make_response
+from flask import Flask, request, g, render_template, make_response, url_for
 from flask_bootstrap import Bootstrap
 from forms import AddDrink
 
@@ -54,6 +54,40 @@ def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
+@app.route('/', methods = ['GET'])
+def list_routes():
+""" lists all routes that exist in this app """
+    def has_no_empty_params(rule):
+        defaults = rule.defaults if rule.defaults is not None else ()
+        arguments = rule.arguments if rule.arguments is not None else ()
+        return len(defaults) >= len(arguments)
+
+    links = []
+    for rule in app.url_map.iter_rules():
+        # Filter out rules we can't navigate to in a browser
+        # and rules that require parameters
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    # links is now a list of url, endpoint tuples
+    from flask import render_template_string
+    tmpl="""
+    <html>
+        <head><title>list of endpoints for debugging</title></head>
+        <body><h1>frauk</h1><h2>list of endpoints</h2>
+        <ul>
+        {% for route in routes %}
+            <li>
+            <a href="{{route[0]}}">{{route[1]}} ({{route[0]}})</a>
+            </li>
+        {% endfor %}
+        </ul>
+    </html>
+    """
+    return render_template_string(tmpl, routes=links)
+    
+
 
 @app.route('/add_drink', methods = ['GET','POST'])
 def add_drink():
