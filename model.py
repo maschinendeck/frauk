2823c000b9d6ec74b407c56c52b3281729e979ea
Vann <poniesofgore@gmail.com>
2017-06-29 20:54:30 +0200

    SqlAlchemy DB Declaration

---
+++ model.py
@@ -0,0 +1,29 @@
+from frauk import db
+class Audit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    createt_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    difference = db.Column(db.Numeric, default=0)
+    drink = ? foreign key stuff
+    user = ?
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    bottle_size_ml = db.Column(db.Integer)
+    caffeine_mg = db.Column(db.Integer)
+    price = db.Column(db.Numeric)
+    logo = db.Column(db.LargeBinary)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    logo_updated_at = db.Column(DateTime)
+    active = db.Column(db.Boolean)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    email = db.Column(db.String(250))
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.now())
+    balance = db.Column(db.Numeric, default=0)
+    active = db.Column(db.Boolean, default=True)
+    anonymous_audits = db.Column(db.Boolean, default=True)
