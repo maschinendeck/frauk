db61c332cc1af4815e716bd0a2c5310bf2b26e2b
towa <tw@cock.li>
2017-07-15 00:17:59 +0200

    colors based on username

--- app/model.py
+++ app/model.py
@@ -1,5 +1,6 @@
 import datetime
 from app import db
+import colors
 
 
 class Audit(db.Model):
@@ -15,17 +16,18 @@ class Drink(db.Model):
     bottle_size_l = db.Column(db.Float)
     caffeine_mg = db.Column(db.Integer)
     price = db.Column(db.Numeric)
-    color = db.Column(db.String(6), default='56bf8b')
+    color = db.Column(db.String(6), default='#56bf8b')
     created_at = db.Column(db.DateTime, default=datetime.datetime.now())
     updated_at = db.Column(db.DateTime, default=datetime.datetime.now())
     active = db.Column(db.Boolean)
 
     def fromForm(self, form):
-        self.name = form.name.data
+        if self.name != form.name.data:
+            self.name = form.name.data
+            self.color = colors.from_name(form.name.data)
         self.bottle_size_l = form.bottle_size_l.data
         self.caffeine_mg = form.caffeine_mg.data
         self.price = form.price.data
-        self.color = form.color.data
         self.updated_at = datetime.datetime.now()
 
 class User(db.Model):
@@ -37,12 +39,13 @@ class User(db.Model):
     balance = db.Column(db.Numeric, default=0)
     active = db.Column(db.Boolean, default=True)
     audit = db.Column(db.Boolean, default=True)
-    color = db.Column(db.String(6), default='56bf8b')
+    color = db.Column(db.String(7), default='#56bf8b')
 
     def fromForm(self,form):
-        self.username = form.username.data
+        if self.username != form.username.data:
+            self.username = form.username.data
+            self.color = colors.from_name(form.username.data)
         self.email = form.email.data
         self.balance = form.balance.data
         self.audit = form.audit.data
-        self.color = form.color.data
         self.updated_at = datetime.datetime.now()
