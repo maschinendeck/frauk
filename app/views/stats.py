e424e627743435f35aafdabd269a74920f2b7978
towa <tw@cock.li>
2017-07-16 20:45:15 +0200

    add daily/monthly caffeine usage to stats

--- app/views/stats.py
+++ app/views/stats.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from ..model import Audit, Drink, User
 from app import db
+import datetime
 
 stats = Blueprint('stats', __name__, url_prefix='/stats')
 
@@ -16,4 +17,19 @@ def get_stats(uid = None):
         group_by(Audit.user_id).\
         order_by(db.desc(db.func.count(Audit.user_id))).\
         limit(5).all()
-    return render_template('stats.html', drinks = drinks, users = users)
+    
+    startOfMonth = datetime.datetime.now().replace(day=1)
+    startOfDay = datetime.datetime.now().replace(hour=0,minute=0,second=0)
+    tomorrow = datetime.datetime.now() + datetime.timedelta(days=1)
+
+    caff_month = Audit.query.join(Drink).filter(Audit.created_at > startOfMonth)
+    sum_month = sum([c.drink.caffeine_mg * c.drink.bottle_size_l * 10 for c in caff_month])
+
+    caff_day = Audit.query.join(Drink).filter(Audit.created_at > startOfDay)
+    sum_day = sum([c.drink.caffeine_mg * c.drink.bottle_size_l * 10 for c in caff_day])
+
+    return render_template('stats.html',
+        drinks = drinks,
+        users = users,
+        caff_month = sum_month,
+        caff_day = sum_day)
