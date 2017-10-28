c197c2447af11398eda816ba4ce642a76186077c
towa <tw@cock.li>
2017-10-28 23:49:02 +0200

    add pagination to audits

--- frauk/views/audits.py
+++ frauk/views/audits.py
@@ -10,4 +10,4 @@ def get_audits(uid = None):
     if uid:
         # this filters on the last joined table. therefore id is User.id
         audits = audits.filter_by(id=uid)
-    return render_template('audits.html', audits = audits)
+    return render_template('audits.html', query = audits.paginate())
