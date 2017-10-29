7708d5e77508b3fa8a71fb8ef30f91600a118bcb
towa <tw@cock.li>
2017-10-29 11:31:36 +0100

    filtering for audits

--- frauk/views/audits.py
+++ frauk/views/audits.py
@@ -1,13 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from ..model import Audit
 
 audits = Blueprint('audits', __name__, url_prefix='/audits')
 
 @audits.route('/', methods = ['GET'])
-@audits.route('/<uid>', methods = ['GET'])
-def get_audits(uid = None):
-    audits = Audit.query.join('drink').outerjoin('user')
-    if uid:
-        # this filters on the last joined table. therefore id is User.id
-        audits = audits.filter_by(id=uid)
+def get_audits():
+    audits = Audit.query
+    if request.args.get('user'):
+        audits = audits.filter_by(user_id=request.args.get('user'))
+    if request.args.get('drink'):
+        audits = audits.filter_by(drink_id=request.args.get('drink'))
+        
     return render_template('audits.html', query = audits.paginate())
