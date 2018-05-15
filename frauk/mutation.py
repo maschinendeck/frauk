8df746b8d837f900592c8a7e177b98c00ee6ee5d
towa <tw@cock.li>
2018-05-15 13:10:30 +0200

    add createUser and modifyUser Mutations

--- frauk/mutation.py
+++ frauk/mutation.py
@@ -1,6 +1,9 @@
 import graphene
 from frauk.audit.mutations import CreateDiffAudit, CreateProductAudit
+from frauk.user.mutations import CreateUser, ModifyUser
 
 class Mutation(graphene.ObjectType):
     create_product_audit = CreateProductAudit.Field()
     create_diff_audit = CreateDiffAudit.Field()
+    create_user = CreateUser.Field()
+    modify_user = ModifyUser.Field()
