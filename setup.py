4e9297aafb0c932feb24c116754957a175e33e98
vann <poniesofgore@gmail.com>
2018-05-21 10:21:32 +0200

    setup dependencies

--- setup.py
+++ setup.py
@@ -3,15 +3,17 @@ from setuptools import setup
 setup(
     name="frauk",
     version="0.0.1",
-    packages=['frauk'],
+    packages=['backend'],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
         'flask',
+        'flask-cors',
+        'graphene',
+        'graphene-sqlalchemy',
+        'marshmallow-sqlalchemy',
+        'flask-graphql',
         'flask_sqlalchemy==2.1',
-        'flask_wtf',
-        'flask_bootstrap',
-        'fnvhash',
-        'pillow'
+        'flask-marshmallow'
     ]
 )
